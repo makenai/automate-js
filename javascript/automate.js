e2fe2fac17851539c061ebcfbd0ccca382dd7601
Pawel Szymczykowski <makenai@gmail.com>
2015-01-11 10:34:54 -0800

    Now drawing on canvas

--- javascript/automate.js
+++ javascript/automate.js
@@ -2,21 +2,48 @@ $(function() {
 
   var canvas = document.getElementById('surface'),
       context = canvas.getContext('2d'),
-      isDrawing = false;
+      isDrawing = false,
+      segmentCount = 0;
+
+  // for storing coordinates
+  var coordinates = [];
 
   $(canvas).on('mousedown', function() {
     isDrawing = true;
   }).on('mouseup', function() {
     isDrawing = false;
-  }).on('mouseleave', function() {
-    isDrawing = false;
-  }).on('mousemove', function() {
-    console.log('mousemove');
+    segmentCount = 0;
+  }).on('mousemove', function(e) {
+    if ( isDrawing ) {
+      coordinates.push({
+        x: e.clientX - canvas.offsetLeft,
+        y: e.clientY - canvas.offsetTop,
+        lineStart: segmentCount == 0
+      });
+      segmentCount++;
+    }
+    redraw();
   });
+
+  function redraw() {
+    context.beginPath();
+    for (i in coordinates) {
+      var c = coordinates[i];
+      if ( c.lineStart ) {
+        context.moveTo( c.x, c.y );
+      } else {
+        context.lineTo( c.x, c.y );
+      }
+    }
+    context.lineWidth = 1;
+    context.strokeStyle = 'black';
+    context.stroke();
+  }
 
   function init() {
     canvas.width = 500;
     canvas.height = canvas.width * 0.75;
+    redraw();
   }
   init();
 
