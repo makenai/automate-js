b0fc10dfd7020897340c9e4828ce763282188c04
Pawel Szymczykowski <makenai@gmail.com>
2015-01-12 23:11:27 -0800

    Graphing XY

--- javascript/automate.js
+++ javascript/automate.js
@@ -1,14 +1,18 @@
 $(function() {
 
-  var canvas = document.getElementById('surface'),
-      context = canvas.getContext('2d'),
+  var AXIS_SCALE = 0.25;
+
+  var drawCanvas = document.getElementById('drawingSurface'),
+      drawCtx = drawCanvas.getContext('2d'),
+      displayCanvas = document.getElementById('display'),
+      displayCtx = displayCanvas.getContext('2d'),
       isDrawing = false,
       segmentCount = 0;
 
-  // for storing coordinates
   var coordinates = [];
 
-  $(canvas).on('mousedown', function() {
+  // Drawing Functionality
+  $(drawCanvas).on('mousedown', function() {
     isDrawing = true;
   }).on('mouseup', function() {
     isDrawing = false;
@@ -16,8 +20,8 @@ $(function() {
   }).on('mousemove', function(e) {
     if ( isDrawing ) {
       coordinates.push({
-        x: e.clientX - canvas.offsetLeft,
-        y: e.clientY - canvas.offsetTop,
+        x: e.clientX - drawCanvas.offsetLeft,
+        y: e.clientY - drawCanvas.offsetTop,
         lineStart: segmentCount == 0
       });
       segmentCount++;
@@ -26,23 +30,31 @@ $(function() {
   });
 
   function redraw() {
-    context.beginPath();
+    drawCtx.beginPath();
     for (i in coordinates) {
       var c = coordinates[i];
       if ( c.lineStart ) {
-        context.moveTo( c.x, c.y );
+        drawCtx.moveTo( c.x, c.y );
       } else {
-        context.lineTo( c.x, c.y );
+        drawCtx.lineTo( c.x, c.y );
       }
+      displayCtx.fillStyle = "#FF0000";
+      displayCtx.fillRect( i, 0, 1, c.x * AXIS_SCALE);
+
+      displayCtx.fillStyle = "#00FF00";
+      displayCtx.fillRect( i, 150, 1, c.y * AXIS_SCALE);
+
     }
-    context.lineWidth = 1;
-    context.strokeStyle = 'black';
-    context.stroke();
+    drawCtx.lineWidth = 1;
+    drawCtx.strokeStyle = 'black';
+    drawCtx.stroke();
   }
 
   function init() {
-    canvas.width = 500;
-    canvas.height = canvas.width * 0.75;
+    drawCanvas.width = 500;
+    drawCanvas.height = drawCanvas.width * 0.75;
+    displayCanvas.width = 500;
+    displayCanvas.height = drawCanvas.width * 0.75;
     redraw();
   }
   init();
